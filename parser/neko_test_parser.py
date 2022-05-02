d919194f9118840dfcd8a2cb358ba27cb655145a
Turchanov Denis <turchanov_denis@mail.ru>
2022-05-03 01:40:42 +0700

    prepare template

--- parser/neko_test_parser.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = 'https://bik.sfu-kras.ru/elib/view?id=BOOK1-84%284%D0%90%29-444/%D0%9C%20805-521870'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-print(soup.find_all('li'))
--- parser/parser_main.py
+++ parser/parser_main.py
@@ -3,14 +3,21 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtGui
 from PyQt5 import QtCore, QtWidgets
-
+from layout.main_layout import Ui_MainWindow
+from layout.template_layout import Dialog_get_date
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setupUi(self)
         self.show()
+        self.serfing_button.clicked.connect(self.get_parse_date)
+
+    def get_parse_date(self):
+        self.dialog_date = Dialog_get_date([self.geometry().x(), self.geometry().y()])
+        self.dialog_date.exec_()
 
 
 if __name__ == '__main__':

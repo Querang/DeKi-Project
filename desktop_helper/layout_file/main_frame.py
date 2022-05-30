2381d3909aef54d4a7e7ecf54c89e0845c6dc406
Turchanov Denis <turchanov_denis@mail.ru>
2022-05-31 00:39:48 +0700

    move to helper

--- desktop_helper/layout_file/main_frame.py
+++ desktop_helper/layout_file/main_frame.py
@@ -162,13 +162,126 @@ class Ui_MainFrame(object):
         self.teg_list_frame.hide()
         self.tag_button_flag = False
         """button connect"""
-        self.tag_button.clicked.connect(self.tag_button_switch)
+
 
         return self.page_main_frame
 
-    def tag_button_switch(self):
-        if self.tag_button_flag:
-            self.teg_list_frame.hide()
+    def append_icon_button(self):
+        pass
+
+class FlowLayout(QtWidgets.QLayout):
+    def __init__(self, parent=None, margin=20, hspacing=15, vspacing=20):
+        super(FlowLayout, self).__init__(parent)
+        self._hspacing = hspacing
+        self._vspacing = vspacing
+        self._items = []
+        self.setContentsMargins(margin, 0, margin, margin)
+
+    def __del__(self):
+        del self._items[:]
+
+    def addItem(self, item):
+        self._items.append(item)
+
+    def horizontalSpacing(self):
+        if self._hspacing >= 0:
+            return self._hspacing
         else:
-            self.teg_list_frame.show()
-        self.tag_button_flag = not self.tag_button_flag
+            return self.smartSpacing(
+                QtWidgets.QStyle.PM_LayoutHorizontalSpacing)
+
+    def verticalSpacing(self):
+        if self._vspacing >= 0:
+            return self._vspacing
+        else:
+            return self.smartSpacing(
+                QtWidgets.QStyle.PM_LayoutVerticalSpacing)
+
+    def count(self):
+        return len(self._items)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self._items):
+            return self._items[index]
+
+    def takeAt(self, index):
+        if 0 <= index < len(self._items):
+            return self._items.pop(index)
+
+    def expandingDirections(self):
+        return QtCore.Qt.Orientations(0)
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        return self.doLayout(QtCore.QRect(0, 0, width, 0), True)
+
+    def setGeometry(self, rect):
+        super(FlowLayout, self).setGeometry(rect)
+        self.doLayout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QtCore.QSize()
+        for item in self._items:
+            size = size.expandedTo(item.minimumSize())
+        left, top, right, bottom = self.getContentsMargins()
+        size += QtCore.QSize(left + right, top + bottom)
+        return size
+
+    def doLayout(self, rect, testonly):
+        left, top, right, bottom = self.getContentsMargins()
+        effective = rect.adjusted(+left, +top, -right, -bottom)
+        x = effective.x()
+        y = effective.y()
+        lineheight = 0
+        for item in self._items:
+            widget = item.widget()
+            hspace = self.horizontalSpacing()
+            if hspace == -1:
+                hspace = widget.style().layoutSpacing(
+                    QtWidgets.QSizePolicy.PushButton,
+                    QtWidgets.QSizePolicy.PushButton, QtCore.Qt.Horizontal)
+            vspace = self.verticalSpacing()
+            if vspace == -1:
+                vspace = widget.style().layoutSpacing(
+                    QtWidgets.QSizePolicy.PushButton,
+                    QtWidgets.QSizePolicy.PushButton, QtCore.Qt.Vertical)
+            nextX = x + item.sizeHint().width() + hspace
+            if nextX - hspace > effective.right() and lineheight > 0:
+                x = effective.x()
+                y = y + lineheight + vspace
+                nextX = x + item.sizeHint().width() + hspace
+                lineheight = 0
+            if not testonly:
+                item.setGeometry(
+                    QtCore.QRect(QtCore.QPoint(x, y), item.sizeHint()))
+            x = nextX
+            lineheight = max(lineheight, item.sizeHint().height())
+        return y + lineheight - rect.y() + bottom
+
+    def smartSpacing(self, pm):
+        parent = self.parent()
+        if parent is None:
+            return -1
+        elif parent.isWidgetType():
+            return parent.style().pixelMetric(pm, None, parent)
+        else:
+            return parent.spacing()
+
+class IconButton(QtWidgets.QPushButton):
+    def __init__(self):
+        super(IconButton, self).__init__()
+        self.setEnabled(True)
+        self.setStyleSheet("background: rgba(23, 23, 23, 0.0);\n"
+                                      "box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);\n"
+                                      "border-radius: 13px;")
+        self.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("material/image 42.png"), QtGui.QIcon.Normal,
+                       QtGui.QIcon.Off)
+        self.setIcon(icon)
+        self.setIconSize(QtCore.QSize(50, 50))
